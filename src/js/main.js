0376a0c88f745246d1a3ea5bae169e9ac6561bdd
elisemutrux <122873450+elisemutrux@users.noreply.github.com>
2024-06-12 14:20:48 +0200

    pop up produits

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,31 @@
-var popupOpen = document.querySelector(".products-background-brune");
-var popupBox = document.querySelector(".popup-beer");
-var popupClose = document.querySelector(".popup-beer-cross");
+var popupBruneOpen = document.querySelector(".products-background-brune");
+var popupBlondeOpen = document.querySelector(".products-background-blonde");
+var popupIpaOpen = document.querySelector(".products-background-ipa");
 
-popupOpen.addEventListener("click", function (toogle) {
-  popupBox.classList.add("is-open");
+var popupBruneBox = document.querySelector(".popup-beer.t-brune");
+var popupBlondeBox = document.querySelector(".popup-beer.t-blonde");
+var popupIpaBox = document.querySelector(".popup-beer.t-ipa");
+
+var popupBruneClose = document.querySelector(".popup-beer-cross.t-brune");
+var popupBlondeClose = document.querySelector(".popup-beer-cross.t-blonde");
+var popupIpaClose = document.querySelector(".popup-beer-cross.t-ipa");
+
+popupBruneOpen.addEventListener("click", function (toogle) {
+  popupBruneBox.classList.add("is-open");
+});
+popupBlondeOpen.addEventListener("click", function (toogle) {
+  popupBlondeBox.classList.add("is-open");
+});
+popupIpaOpen.addEventListener("click", function (toogle) {
+  popupIpaBox.classList.add("is-open");
 });
 
-popupClose.addEventListener("click", function (toogle) {
-  popupBox.classList.remove("is-open");
+popupBruneClose.addEventListener("click", function (toogle) {
+  popupBruneBox.classList.remove("is-open");
+});
+popupBlondeClose.addEventListener("click", function (toogle) {
+  popupBlondeBox.classList.remove("is-open");
+});
+popupIpaClose.addEventListener("click", function (toogle) {
+  popupIpaBox.classList.remove("is-open");
 });
